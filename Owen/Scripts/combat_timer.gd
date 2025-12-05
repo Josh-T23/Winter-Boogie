221ffe8877d899da77a49d6266f34e986bfe7d13
OwenZimmer1 <ozimmer@uwm.edu>
2025-12-05 12:39:17 -0600

    Not crash anymore

--- Owen/Scripts/combat_timer.gd
+++ Owen/Scripts/combat_timer.gd
@@ -6,6 +6,7 @@ func _on_timeout() -> void:
 	if Global.round == 3:
 		Global.reset()
 		get_tree().change_scene_to_file("res://starting_scene.tscn")
+		return
 	Global.round += 1
 	Global.weapon = null
 	get_tree().change_scene_to_file("res://owen_human_joystick.tscn")
