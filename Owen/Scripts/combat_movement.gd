097dd0d1af60b33a3297f4982128ad69a7a54459
OwenZimmer1 <ozimmer@uwm.edu>
2025-12-08 21:02:26 -0600

    combat movement

---
+++ Owen/Scripts/combat_movement.gd
@@ -0,0 +1,10 @@
+extends XROrigin3D
+
+var speed = 2
+
+func _process(delta: float) -> void:
+	var push_x = $XRCamera3D.global_position.x - $Left.global_position.x
+	var push_z = $XRCamera3D.global_position.z - $Left.global_position.z
+	
+	position.x += push_x * delta * speed
+	position.z += push_z * delta * speed
