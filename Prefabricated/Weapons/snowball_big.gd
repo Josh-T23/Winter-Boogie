e35bf377bedafe3b770366900c547987bd014da7
Josh-T23 <thaisan2@uwm.edu>
2025-12-17 19:39:22 -0600

    Fixed Weapon Switching Menu

--- Prefabricated/Weapons/snowball_big.gd
+++ Prefabricated/Weapons/snowball_big.gd
@@ -14,7 +14,7 @@ func _on_body_entered(body):
 func _on_area_3d_body_entered(body):
 	if body.name.contains("SnowmanReal"):
 		#print("Detecting a snowman!")
-		Global.score += 1
+		Global.score += 4
 		var rootScene = get_tree().root.get_child(1)
 		var timesSpawn = 3
 		while(timesSpawn != 0):
